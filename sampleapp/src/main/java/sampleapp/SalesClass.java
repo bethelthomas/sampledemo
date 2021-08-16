d07f3a11c7e027e476dd36125959e84ca9e74393
betthoma <betthoma@LIN25005217.corp.capgemini.com>
2021-08-16 15:15:01 +0530

    second commit

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -2,7 +2,7 @@ package sampleapp;
 
 public class SalesClass {
 	public void display() {
-		int[] arr= {23,45,56,67,78,9};
+		int[] arr= {23,45,56,67,78,91};
 		for(int num:arr)
 			System.out.println(num);
 	}
