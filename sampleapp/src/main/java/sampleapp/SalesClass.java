465988d4d84825949d53560cac535d94a65a63c4
betthoma <betthoma@LIN25005217.corp.capgemini.com>
2021-08-17 11:07:45 +0530

    commit

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -3,10 +3,8 @@ package sampleapp;
 public class SalesClass {
 	public void display() {
 		int[] arr= {23,45,56,67,78,91};
-		for(int i=0;i<arr.length;i++)
-			System.out.println(arr[i]);
-		
-		System.out.println("Array size:"+arr.length);
+		for(int num:arr)
+			System.out.println(num);
 	}
 	
 	public void print() {
